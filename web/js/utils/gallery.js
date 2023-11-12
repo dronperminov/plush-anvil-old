aa3c558102439d79d9452962277f1c93a289a64a
dronperminov <perminov@ispras.ru>
2023-11-13 01:02:29 +0300

    add loader for photos

--- web/js/utils/gallery.js
+++ web/js/utils/gallery.js
@@ -15,6 +15,13 @@ const GALLERY_RIGHT_ICON = `<svg width="30px" height="30px" viewBox="-100 -100 1
     <path d="M256 120.768L306.432 64 768 512l-461.568 448L256 903.232 659.072 512z" />
 </svg>`
 
+const GALLERY_LOADER_ICON = `<svg xmlns="http://www.w3.org/2000/svg" width="62px" height="62px" viewBox="-20 -20 296 296" fill="#000">
+    <path d="M248,91.3V67H80v8H9c0,0,10.7,40.6,67.3,40.6c30.3,0,34.4,12.7,34.4,19.1c0,8.4-5.1,21.9-36.7,32.8V191h38.7
+    c6.8-5.2,15.3-8.2,24.5-8.2s17.7,3.1,24.5,8.2H201c0,0,0-15.1,0-22.9c-23.4-7.7-38.7-20.4-38.7-34.8
+    C162.3,110.6,200.1,92.5,248,91.3z M80,87c-52,0-52-4-52-4h52C80,83,80,85.4,80,87z M88,79v-4h152v4H88z"/>
+    <animateTransform xmlns="http://www.w3.org/2000/svg" attributeName="transform" type="rotate" from="0 0 0" to="360 0 0" dur="0.9s" repeatCount="indefinite"/>
+</svg>`
+
 function Gallery(popupId) {
     this.popup = document.getElementById(popupId)
     this.body = document.getElementsByTagName("body")[0]
@@ -59,8 +66,10 @@ Gallery.prototype.BuildPhotos = function() {
         let originalUrl = image.getAttribute("data-src")
 
         let photo = this.MakeElement("gallery-photo", this.gallery)
-        let photoImage = this.MakeElement("gallery-photo-image", photo, {src: previewUrl, alt: alt}, "img")
-        this.photos.push({original: originalUrl, image: photoImage})
+        let photoImage = this.MakeElement("gallery-photo-image", photo, {alt: alt}, "img")
+        let loader = this.MakeElement("gallery-loader", photo, {innerHTML: GALLERY_LOADER_ICON})
+        photoImage.addEventListener("load", () => loader.remove())
+        this.photos.push({original: originalUrl, image: photoImage, loader: loader})
     }
 
     this.photoIndex = 0
