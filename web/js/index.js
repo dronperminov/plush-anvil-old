3545f0eb5ca2808d0e4bfa8ba387245fe0b5018c
dronperminov <perminov@ispras.ru>
2024-02-01 15:06:56 +0300

    add clipboard copying of poll header

--- web/js/index.js
+++ web/js/index.js
@@ -2,6 +2,10 @@ const PHOTO_ICON = `<svg width="25px" height="25px" viewBox="0 0 512 512" xmlns=
     <path d="M277.333333,1.42108547e-14 C277.333333,1.42108547e-14 277.681778,0.557511111 278.378667,1.67253333 L278.698667,2.18453333 C279.438222,3.36782222 280.478222,5.03182222 281.818667,7.17653333 L282.458667,8.20053333 C283.891556,10.4931556 285.624889,13.2664889 287.658667,16.5205333 L288.618667,18.0565333 C290.744889,21.4584889 293.171556,25.3411556 295.898667,29.7045333 L297.178667,31.7525333 C299.998222,36.2638222 303.118222,41.2558222 306.538667,46.7285333 L308.138667,49.2885333 C311.003462,53.8722052 314.068069,58.775577 317.332489,63.9986486 L426.666667,64 L426.666667,362.666667 L7.10542736e-15,362.666667 L7.10542736e-15,64 L109.333,64 L149.333333,1.42108547e-14 L277.333333,1.42108547e-14 L277.333333,1.42108547e-14 Z M213.333333,85.3333333 C148.531923,85.3333333 96,137.865256 96,202.666667 C96,267.468077 148.531923,320 213.333333,320 C278.134744,320 330.666667,267.468077 330.666667,202.666667 C330.666667,137.865256 278.134744,85.3333333 213.333333,85.3333333 Z M213.333333,128 C254.570595,128 288,161.429405 288,202.666667 C288,243.903928 254.570595,277.333333 213.333333,277.333333 C172.096072,277.333333 138.666667,243.903928 138.666667,202.666667 C138.666667,161.429405 172.096072,128 213.333333,128 Z M384,106.666667 C372.217925,106.666667 362.666667,116.217925 362.666667,128 C362.666667,139.782075 372.217925,149.333333 384,149.333333 C395.782075,149.333333 405.333333,139.782075 405.333333,128 C405.333333,116.217925 395.782075,106.666667 384,106.666667 Z" transform="translate(42.666667, 64.000000)" />
 </svg>`
 
+const POLL_ICON = `<svg width="25px" height="25px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path d="M7 11h7v2H7zm0-4h10.97v2H7zm0 8h13v2H7zM4 4h2v16H4z"/>
+</svg>`
+
 function CloseAllDetails() {
     for (let details of document.getElementsByClassName("quiz-details"))
         details.classList.add("hidden")
@@ -156,6 +160,32 @@ function BuildScheduleCells(schedule, places, isAdmin) {
     }
 }
 
+function CopyPollHeader(quiz, weekday, places) {
+    const weekday2str = {
+        "понедельник": "Пн",
+        "вторник": "Вт",
+        "среда": "Ср",
+        "четверг": "Чт",
+        "пятница": "Пт",
+        "суббота": "Сб",
+        "воскресенье": "Вс",
+    }
+
+    let day = `${quiz.date.day}`.padStart(2, '0')
+    let month = `${quiz.date.month}`.padStart(2, '0')
+    let name = quiz.name.replace(/\.$/g, "")
+
+    let headerDate = `${day}.${month} ${weekday2str[weekday]} ${quiz.time}`
+    let headerPlace = `${quiz.place} (м. ${places[quiz.place].metro_station}) ${quiz.cost} руб.`
+    let headerLength = headerDate.length + headerPlace.length
+
+    if (headerDate.length + headerPlace.length + name.length >= 240)
+        name = quiz.short_name.replace(/\.$/g, "")
+
+    let header = `${headerDate} ${name}. ${headerPlace}`
+    navigator.clipboard.writeText(header)
+}
+
 function BuildScheduleDetails(schedule, places, isAdmin) {
     let scheduleBlock = document.getElementById("schedule")
 
@@ -171,6 +201,8 @@ function BuildScheduleDetails(schedule, places, isAdmin) {
                 if (isAdmin) {
                     let icon = MakeElement("schedule-quiz-album interactive-fill-icon", content, {innerHTML: PHOTO_ICON})
                     icon.addEventListener("click", () => location.href = `/quiz-album/${quiz._id}`)
+                    let poll = MakeElement("schedule-quiz-poll interactive-fill-icon", content, {innerHTML: POLL_ICON})
+                    poll.addEventListener("click", () => CopyPollHeader(quiz, cell.weekday, places))
                 }
 
                 MakeElement("quiz-details-name", content, {innerText: quiz.name})
